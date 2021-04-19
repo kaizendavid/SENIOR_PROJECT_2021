5b41022ae2b0e1890d009589041cce1792fd4857
David Rodriguez <davidRodriguez2@csus.edu>
2021-04-18 18:58:10 -0700

    Changed ports back to 5000

--- fall2020_cat/server/server.js
+++ fall2020_cat/server/server.js
@@ -112,7 +112,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
